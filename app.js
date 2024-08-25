ef5e5dbd1ab153882c150a97eb73e6cba39155cf
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-08-25 11:49:21 +0500

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+// Assignment : 01 VARIABLES FOR NUMBERS
+
+// Question : 01
+
+// var age = ("I am 18 year old")
+// alert(age)
+
+// Question : 02
+
+// var visited = ("Your have visited this site 14 time")
+// alert(visited)
+
+// Question : 03
+
+// var birthYear = ("My birth year is 2005 <br/> Data type of my declared variable is number");
+// document.write(birthYear);
+
+// Question : 04
+
+// var onlineStore = ("<b>John Doe</b> ordered 5 <b>T-shirt's</b> on <b>XYZ</b> Clothing store");
+// document.write(onlineStore );
